7556fcdfc45c17c1320078122131101ebe70dfc2
nikhilsubsun321@gmail.com <nikhilsubsun321@gmail.com>
2025-12-24 22:53:05 +0530

    Mongodb

---
+++ api/test-db.js
@@ -0,0 +1,18 @@
+import clientPromise from "../lib/mongodb";
+
+export default async function handler(req, res) {
+  try {
+    const client = await clientPromise;
+    const db = client.db("quantumchem");
+
+    res.status(200).json({
+      ok: true,
+      message: "MongoDB connected successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      error: error.message,
+    });
+  }
+}
