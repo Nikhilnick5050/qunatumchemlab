12d46dfdfa13e338c37cf40209ed943173ad7b61
nikhilsubsun321@gmail.com <nikhilsubsun321@gmail.com>
2025-12-24 22:40:40 +0530

    Temp

--- api/index.js
+++ api/index.js
@@ -1,15 +1,6 @@
-import clientPromise from "../lib/mongodb";
-
-export default async function handler(req, res) {
-  try {
-    const client = await clientPromise;
-    const db = client.db();
-
-    res.status(200).json({
-      ok: true,
-      db: db.databaseName,
-    });
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    message: "API is alive"
+  });
 }
